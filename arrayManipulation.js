33632ddf615a687e320bae0182aa958a593740fd
ADK <adk.kda10@gmail.com>
2024-05-17 20:03:13 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -6,4 +6,20 @@ let arrayOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let newArray = processArray(arrayOfNumbers);
 console.log(newArray);
 
+  
+function formatArrayStrings(strings, arrayOfNumbers) {
+  let modifiedStrings = [];
+for (let i = 0; i < strings.length; i++) {
+   if (arrayOfNumbers[i] % 2 === 0 ) {
+    modifiedStrings.push(strings[i].toUpperCase());
+} else {
+    modifiedStrings.push(strings[i].toLowerCase());
+        };
+};
+return modifiedStrings;
+};
+      
+let strings = ["Gummies", "Skittles", "Mars", "Bounty", "Caramel", "Cardberry", "Minigems"];
+let modifiedStrings = formatArrayStrings(strings, arrayOfNumbers);
+console.log(modifiedStrings);
 
